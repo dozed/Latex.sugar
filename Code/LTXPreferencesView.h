11576172d761d48594d525085ccc94ece77a37b6
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2011-03-15 01:03:11 +0100

    Created a preferences view for the sugar.

---
+++ Code/LTXPreferencesView.h
@@ -0,0 +1,16 @@
+//
+//  Test.h
+//  Latex
+//
+//  Created by Stefan Ollinger on 14.03.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+@interface LTXPreferencesViewController : NSViewController
+{
+	NSButton *showCompileErrors;
+}
+
+@property (readonly,retain) IBOutlet NSButton *showCompileErrors;
+
+@end
---
+++ Code/LTXPreferencesView.m
@@ -0,0 +1,15 @@
+//
+//  LTXPreferences.m
+//  Latex
+//
+//  Created by Stefan Ollinger on 14.03.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LTXPreferencesView.h"
+
+@implementation LTXPreferencesViewController
+
+@synthesize showCompileErrors;
+
+@end
